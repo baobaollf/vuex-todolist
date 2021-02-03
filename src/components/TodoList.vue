2cea2789c0cf66bfbdc945c2714c2f45488a6c89
baobaollf <55776365+baobaollf@users.noreply.github.com>
2021-02-03 11:57:07 -0500

    added add button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,6 +5,17 @@
 
 <template>
   <div>
+    <h3>
+      add new todo...
+    </h3>
+    <div>
+      <form>
+        <input type="text" name="input" placeholder="type here..." v-model="input">
+
+      </form>
+      <button v-on:click="addItemLocal(input)">add</button> <br>
+    </div>
+
     <ol>
       <p v-for="(item, index) in this.getTodos" v-bind:key="index">
         {{ item.item }}
@@ -24,11 +35,20 @@ import {mapGetters, mapActions} from 'vuex';
 
 export default {
   name: "TodoList",
+  data() {
+    return {
+      input: "",
+    }
+  },
   methods: {
     ...mapActions([
       'addItem',
       'deleteItem'
     ]),
+    addItemLocal() {
+      this.addItem(this.input);
+      this.input = "";
+    }
   },
 
   computed: {
